fe61a56ea3b8b2e3ca5c2d5495182a917b73e4ac
Mateusz Piotr Moruś <mpmorus@gmail.com>
2020-10-24 12:52:39 +0200

    Klikanie numerków

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,25 +1,57 @@
+import { Console } from "console";
 import React from "react";
 import "./SignUpForm.css";
 
+type SignUpFormState = {
+  patientName: string;
+  patientNumber: number;
+  doctorName: string;
+  doctorNumber: number;
+};
+
 class SignUpForm extends React.Component {
+  state: SignUpFormState = {
+    patientName: "Pacjent",
+    patientNumber: 1,
+    doctorName: "Lekarz",
+    doctorNumber: 1,
+  };
+
+  handleUserTypeButtonPatient = (event: React.MouseEvent): void => {
+    this.setState({
+      patientNumber: this.state.patientNumber + 1,
+    });
+  };
+
+  handleUserTypeButtonDoctor = (event: React.MouseEvent): void => {
+    this.setState({
+      doctorNumber: this.state.doctorNumber + 1,
+    });
+  };
+
   render() {
     return (
       <div className="sign-up-container">
         <h3>Rejestracja</h3>
         <div className="sign-up-container-buttons">
+          <form></form>
           <div>
-            <input
+            <button
               className="user-type-button"
               type="button"
-              value="Jestem pacjentem"
-            />
+              onClick={this.handleUserTypeButtonPatient}
+            >
+              {this.state.patientName + " " + this.state.patientNumber}
+            </button>
           </div>
           <div>
-            <input
+            <button
               className="user-type-button"
               type="button"
-              value="Jestem lekarzem"
-            />
+              onClick={this.handleUserTypeButtonDoctor}
+            >
+              {this.state.doctorName + " " + this.state.doctorNumber}
+            </button>
           </div>
         </div>
       </div>
