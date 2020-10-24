354e800db1010c797b89afa2b8a75cf206bd2b5a
blossomnight <50330117+blossomnight@users.noreply.github.com>
2020-10-24 11:41:22 +0200

    [NEW]SignUpForm

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SignUpForm extends React.Component {
+  render() {
+    return (
+      <div className="sign-up-container">
+        <h3>Rejestracja</h3>
+        <input className="user-type-button" type="button"></input>
+        <input className="user-type-button" type="button"></input>
+      </div>
+    );
+  }
+}
+
+export default SignUpForm;
