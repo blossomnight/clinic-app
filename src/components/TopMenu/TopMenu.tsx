212a7fe561fa980d1f49ca5e011ff9fc81c6297e
blossomnight <50330117+blossomnight@users.noreply.github.com>
2021-01-19 12:17:54 +0100

    [BUILD] CSS Home Screen

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -3,17 +3,38 @@ import "./TopMenu.css";
 
 type TopMenuProps = {
   userName: string;
+  isDropDownVisible: boolean;
 };
 
 class TopMenu extends React.Component {
+  state = {
+    userName: "",
+    isDropDownVisible: false,
+  };
   constructor(props: TopMenuProps) {
     super(props);
   }
 
+  handleShowDropDown = (): void => {
+    this.setState({
+      isDropDownVisible: true,
+    });
+  };
   render() {
     return (
       <div className="top-menu">
-        <div className="user-menu"></div>
+        <div className="user-menu" onClick={this.handleShowDropDown}>
+          <div className="icon-wrapper">
+            <i className="fi-rr-user"></i>
+          </div>
+          <p>Witaj, User23!</p>
+          <div className="icon-wrapper">
+            <i className="fi-rr-menu-dots-vertical"></i>
+          </div>
+        </div>
+        {this.state.isDropDownVisible && (
+          <div className="user-menu-dropdown"></div>
+        )}
       </div>
     );
   }
--- src/screens/Home/ScreenUserHome.tsx
+++ src/screens/Home/ScreenUserHome.tsx
@@ -12,33 +12,35 @@ export class ScreenUserHome extends React.Component<ScreenUserHomeProps> {
       <div className="screen user-home">
         <TopMenu />
         <div className="screen-choice-grid" onClick={this.props.onScreenChoice}>
-          <div>
-            <button
-              name={ScreenType[ScreenType.TypeTestResults]}
-              className="screen-choice-button"
-            >
-              {"Wyniki Badań"}
-            </button>
-            <button
-              name={ScreenType[ScreenType.TypePerscriptions]}
-              className="screen-choice-button"
-            >
-              {"Odbierze E-receptę"}
-            </button>
-          </div>
-          <div>
-            <button
-              name={ScreenType[ScreenType.TypeCreateConsultation]}
-              className="screen-choice-button"
-            >
-              {"Umów wizytę"}
-            </button>
-            <button
-              name={ScreenType[ScreenType.TypeConsultations]}
-              className="screen-choice-button"
-            >
-              {"Umów Konsultację"}
-            </button>
+          <div className="buttons-wrapper">
+            <div>
+              <button
+                name={ScreenType[ScreenType.TypeTestResults]}
+                className="screen-choice-button"
+              >
+                {"Wyniki Badań"}
+              </button>
+              <button
+                name={ScreenType[ScreenType.TypePerscriptions]}
+                className="screen-choice-button"
+              >
+                {"Odbierze E-receptę"}
+              </button>
+            </div>
+            <div>
+              <button
+                name={ScreenType[ScreenType.TypeCreateConsultation]}
+                className="screen-choice-button"
+              >
+                {"Umów wizytę"}
+              </button>
+              <button
+                name={ScreenType[ScreenType.TypeConsultations]}
+                className="screen-choice-button"
+              >
+                {"Umów Konsultację"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
